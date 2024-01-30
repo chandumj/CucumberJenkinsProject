18aacec7b749395c0e134b6a61ca1dcda94f979a
chandu <chandusai2000@gmail.com>
2024-01-30 12:57:22 +0530

    version

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"2a8cc62b-de2a-4261-a95d-709832dd1184","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706596607548,"group":1,"content":"","tags":"","end":1706596627517,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"176db02e-ec40-4f17-8d48-3df643fceb0d","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706599590821,"group":1,"content":"","tags":"","end":1706599604422,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
